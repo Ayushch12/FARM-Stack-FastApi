340f3b0af923f084e87c6f8368bcb5bb8a1b9961
Ayush12 <openclass321@gmail.com>
2024-02-28 12:12:13 +0100

    Working on database, main file

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,16 @@
+#  @bekbrace
+#  FARMSTACK Tutorial - Sunday 13.06.2021
+
+import motor.motor_asyncio
 from model import Todo
 
-#MongoDB driver
-import motor.motor_asyncio
-
-
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
 database = client.TodoList
 collection = database.todo
 
-
 async def fetch_one_todo(title):
-    document = await collection.find_one({"title":title})
+    document = await collection.find_one({"title": title})
     return document
-
 
 async def fetch_all_todos():
     todos = []
@@ -22,18 +20,15 @@ async def fetch_all_todos():
     return todos
 
 async def create_todo(todo):
-    document = Todo
+    document = todo
     result = await collection.insert_one(document)
-    return result
-
-async def  update_todo(title, desc):
-    await collection.update_one({"title":title},{"$set":{
-        "description" : desc
-    }})
-    document = await collection.find_one({"title":title})
     return document
 
+async def update_todo(title, desc):
+    await collection.update_one({"title": title}, {"$set": {"description": desc}})
+    document = await collection.find_one({"title": title})
+    return document
 
-async def  remove_todo(title):
-    await collection.delete_one({"title":title})
+async def remove_todo(title):
+    await collection.delete_one({"title": title})
     return True
--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,41 @@
+#  @bekbrace
+#  FARMSTACK Tutorial - Sunday 13.06.2021
+
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 
-#APP Object
-app = FastAPI()
+from model import Todo
 
-from database import(
+from database import (
     fetch_one_todo,
     fetch_all_todos,
     create_todo,
-     update_todo,
-     remove_todo
-
+    update_todo,
+    remove_todo,
 )
 
-origins = ["https://localhost:3000"]
+# an HTTP-specific exception class  to generate exception information
 
-# CORS middleware in this way, you're allowing requests from specified origins, including credentials, allowing all HTTP methods, and allowing all headers. Adjust these settings according to your specific requirements and security considerations.
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+# what is a middleware? 
+# software that acts as a bridge between an operating system or database and applications, especially on a network.
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials = True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#GET
+
 @app.get("/")
-def read_root():
-    return{"Ping":"Pong"}
+async def read_root():
+    return {"Hello": "World"}
 
 @app.get("/api/todo")
 async def get_todo():
@@ -34,26 +43,29 @@ async def get_todo():
     return response
 
 @app.get("/api/todo/{title}", response_model=Todo)
-async def get_todo_by_id(title):
-    response = fetch_one_todo(title)
+async def get_todo_by_title(title):
+    response = await fetch_one_todo(title)
     if response:
         return response
-    raise HTTPException(404, f"There is no TODO item with this title: {title}")
+    raise HTTPException(404, f"There is no todo with the title {title}")
 
+@app.post("/api/todo/", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong")
 
-#POST
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.put("/api/todo/{title}/", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no todo with the title {title}")
 
-#DELETE
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
-
-#UPDATE
-@app.put("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
-
-
+@app.delete("/api/todo/{title}")
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return "Successfully deleted todo"
+    raise HTTPException(404, f"There is no todo with the title {title}")
